e62a3068de1f157802ff0d749442142a4f583a9e
Zhentian Wan <answer881215@gmail.com>
2022-08-23 20:11:05 +0300

    Create freeze.js

---
+++ src/freeze.js
@@ -0,0 +1,6 @@
+export const deepFreeze = obj => {
+  Object.keys(obj).forEach(prop => {
+    if (typeof obj[prop] === 'object' && !Object.isFrozen(obj[prop])) deepFreeze(obj[prop]);
+  });
+  return Object.freeze(obj);
+};
