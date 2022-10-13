636ac0c82e23de5a2509bb823537a6c3ed098f25
Zhentian Wan <answer881215@gmail.com>
2022-10-13 20:36:57 +0300

    Create string.ts

---
+++ src/types/string.ts
@@ -0,0 +1,14 @@
+/*
+Example:
+type cases = [
+  Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
+  Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abc'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
+  Expect<Equal<StartsWith<'abc', ''>, true>>,
+  Expect<Equal<StartsWith<'abc', ' '>, false>>,
+  Expect<Equal<StartsWith<'', ''>, true>>,
+]
+*/
+export type StartsWith<T extends string, U extends string> = T extends `${U}${string}` ? true: false;
+
