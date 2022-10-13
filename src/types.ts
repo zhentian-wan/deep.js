74d2a1a1489a5df1aac618bd8ab308e89a9a725f
Zhentian Wan <answer881215@gmail.com>
2022-10-13 20:30:34 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+/* Object */
+
 export type DeepPartial<T> = T extends Function
   ? T
   : T extends Array<infer InferredArrayMember>
@@ -5,9 +7,26 @@ export type DeepPartial<T> = T extends Function
     : T extends object
       ? DeepPartialObject<T>
         : T | undefined;
-
-export interface DeepPartialArray<T> extends Array<DeepPartial<T>> {}
-
-export type DeepPartialObject<T> = {
+interface DeepPartialArray<T> extends Array<DeepPartial<T>> {}
+type DeepPartialObject<T> = {
   [Key in keyof T]?: DeepPartial<T[Key]>
+}
+
+/*
+Example: 
+interface Model {
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+}
+
+type cases = [
+  Expect<Equal<PickByType<Model, boolean>, { isReadonly: boolean; isEnable: boolean }>>,
+  Expect<Equal<PickByType<Model, string>, { name: string }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>,
+]
+*/
+export type PickByType<T extends object, U> = {
+  [Key in keyof T as T[Key] extends U ? Key: never]: T[Key]
 }
