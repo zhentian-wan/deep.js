941577a20d13fbc51492437b389d2644dab291e1
Zhentian Wan <answer881215@gmail.com>
2022-10-16 11:51:38 +0300

    Update object.ts

--- src/types/object.ts
+++ src/types/object.ts
@@ -66,6 +66,25 @@ export type PickByType<T extends object, U> = {
 }
 
 /*
+Example:
+interface Model {
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+}
+
+type cases = [
+  Expect<Equal<OmitByType<Model, boolean>, { name: string; count: number }>>,
+  Expect<Equal<OmitByType<Model, string>, { count: number; isReadonly: boolean; isEnable: boolean }>>,
+  Expect<Equal<OmitByType<Model, number>, { name: string; isReadonly: boolean; isEnable: boolean }>>,
+]
+*/
+export type OmitByType<T extends object, U> = {
+  [Key in keyof T as T[Key] extends U ? never: Key]: T[Key]
+}
+
+/*
 Example
 interface User {
   name?: string
