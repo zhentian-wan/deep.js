1facc7f8247fae0d17b2150fbc0ffcf71f896bc2
Zhentian Wan <answer881215@gmail.com>
2022-10-17 09:42:00 +0300

    Create array.ts

---
+++ src/types/array.ts
@@ -0,0 +1,12 @@
+/*
+Example
+type cases = [
+  // @ts-expect-error
+  Shift<unknown>,
+  Expect<Equal<Shift<[]>, []>>,
+  Expect<Equal<Shift<[1]>, []>>,
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<['a', 'b', 'c', 'd']>, ['b', 'c', 'd']>>,
+]
+*/
+export type Shift<T extends any[], ACC = T> = T extends [infer REMOVED, ...(infer REST)] ? REST: [];
