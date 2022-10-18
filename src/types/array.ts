7b79dd01900b55876f42edbe22c5acf9b36dc46d
Zhentian Wan <answer881215@gmail.com>
2022-10-18 21:24:45 +0300

    Update array.ts

--- src/types/array.ts
+++ src/types/array.ts
@@ -1,6 +1,24 @@
 /*
 Example
 type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
+*/
+export type First<T extends any[]> = T extends [infer First, ...infer REST] ? First: never;
+
+/*
+Example
+type cases = [
   // @ts-expect-error
   Shift<unknown>,
   Expect<Equal<Shift<[]>, []>>,
