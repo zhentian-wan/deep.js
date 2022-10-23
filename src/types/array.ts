b1dbc212b7af884d5d3d04b6affb37e3b4572af1
Zhentian Wan <answer881215@gmail.com>
2022-10-23 15:38:44 +0300

    Update array.ts

--- src/types/array.ts
+++ src/types/array.ts
@@ -70,3 +70,21 @@ type cases = [
 */
 export type Reverse<T> = T extends [...infer H, infer T] ? [T, ...Reverse<H>] : [];
 
+
+/*
+Example
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+]
+*/
+export type Zip<T, U> = T extends [infer F1, ...infer R1]
+  ? U extends [infer F2, ...infer R2]
+    ? [[F1, F2], ...Zip<R1, R2>]
+    : []
+  : [];
+
+
