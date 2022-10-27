80d3f3e2d5bc59139a7902f85e2d1867ba571ae4
Zhentian Wan <answer881215@gmail.com>
2022-10-27 09:41:40 +0300

    Update array.ts

--- src/types/array.ts
+++ src/types/array.ts
@@ -1,3 +1,4 @@
+import type { Equal } from './utils';
 /*
 Example
 type cases = [
@@ -111,6 +112,24 @@ export type IsTuple<T> = [T] extends [never]
       ? false
       : true
     : false;
+
+/*
+Example
+type cases = [
+  Expect<Equal<IndexOf<[1, 2, 3], 2>, 1>>,
+  Expect<Equal<IndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 2>>,
+  Expect<Equal<IndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<IndexOf<[string, 1, number, 'a'], number>, 2>>,
+  Expect<Equal<IndexOf<[string, 1, number, 'a', any], any>, 4>>,
+  Expect<Equal<IndexOf<[string, 'a'], 'a'>, 1>>,
+  Expect<Equal<IndexOf<[any, 1], 1>, 1>>,
+]
+*/
+export type IndexOf<T extends any[], U, ACC extends unknown[] = []> = T extends [infer F, ...infer RT]
+  ? Equal<F, U> extends true
+    ? ACC['length']
+    : IndexOf<RT, U, [...ACC, unknown]>
+  : -1;
 
 /*
 Example
