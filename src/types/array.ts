42152ad5afa1d55e997e6c9304caa878ecf071b9
Zhentian Wan <answer881215@gmail.com>
2022-10-28 10:44:10 +0300

    Update array.ts

--- src/types/array.ts
+++ src/types/array.ts
@@ -84,6 +84,22 @@ export type First<T extends any[]> = T extends [infer First, ...infer REST] ? Fi
 /*
 Example
 type cases = [
+  Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
+  Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
+  Expect<Equal<LastIndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<LastIndexOf<[string, 2, number, 'a', number, 1], number>, 4>>,
+  Expect<Equal<LastIndexOf<[string, any, 1, number, 'a', any, 1], any>, 5>>,
+]
+*/
+export type LastIndexOf<T extends any[], U> = T extends [...infer RT, infer L]
+  ? Equal<L, U> extends true
+    ? RT['length']
+    : LastIndexOf<RT, U>
+  : -1;
+
+/*
+Example
+type cases = [
   // @ts-expect-error
   Shift<unknown>,
   Expect<Equal<Shift<[]>, []>>,
