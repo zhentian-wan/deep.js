8ffbebb475b0923325a04f1465177fe702f0a088
Zhentian Wan <answer881215@gmail.com>
2022-10-28 10:09:09 +0300

    Update array.ts

--- src/types/array.ts
+++ src/types/array.ts
@@ -95,6 +95,21 @@ type cases = [
 export type Shift<T extends any[], ACC = T> = T extends [infer REMOVED, ...(infer REST)] ? REST: [];
 
 /*
+Example
+type cases = [
+  Expect<Equal<Join<['a', 'p', 'p', 'l', 'e'], '-'>, 'a-p-p-l-e'>>,
+  Expect<Equal<Join<['Hello', 'World'], ' '>, 'Hello World'>>,
+  Expect<Equal<Join<['2', '2', '2'], 1>, '21212'>>,
+  Expect<Equal<Join<['o'], 'u'>, 'o'>>,
+]
+*/
+export type Join<T extends any[], U extends string | number, ACC extends string = ''> = T extends [infer A extends string, ...infer RT extends string[]]
+  ? RT['length'] extends 0
+    ? `${ACC}${A}`
+    : Join<RT, U ,`${ACC}${A}${U}`>
+  : ACC;
+
+/*
 Example:
 type cases = [
   Expect<Equal<IsTuple<[]>, true>>,
