c4b8a765e90695cb28ad9b06df81868e933ec559
Zhentian Wan <answer881215@gmail.com>
2022-10-30 11:06:51 +0200

    Update number.ts

--- src/types/number.ts
+++ src/types/number.ts
@@ -34,6 +34,39 @@ export type GreaterThan<T extends number, U extends number, ACC extends unknown[
 
 /*
 Example
+type Result1 = | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type Result2 = | 0 | 1 | 2
+type Result3 =
+  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+  | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
+  | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30
+  | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40
+  | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50
+  | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60
+  | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70
+  | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80
+  | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90
+  | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100
+  | 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109 | 110
+  | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119 | 120
+  | 121 | 122 | 123 | 124 | 125 | 126 | 127 | 128 | 129 | 130
+  | 131 | 132 | 133 | 134 | 135 | 136 | 137 | 138 | 139 | 140
+type cases = [
+  Expect<Equal<NumberRange<2, 9>, Result1>>,
+  Expect<Equal<NumberRange<0, 2>, Result2>>,
+  Expect<Equal<NumberRange<0, 140>, Result3>>,
+]
+*/
+export type NumberRange<L extends number | C['length'], H extends number, ACC = never, C extends unknown[] = []> = 
+  C['length'] extends H
+    ? ACC | H
+    : C['length'] extends L
+      ? NumberRange<[...C, unknown]['length'], H, L | ACC, [...C, unknown]>        // keep increasing L and C until C['length'] === H
+      : NumberRange<L, H, ACC, [...C, unknown]>;                                   // keep increasing C until C['length'] === L
+
+
+/*
+Example
 type cases = [
   Expect<Equal<Trunc<0.1>, '0'>>,
   Expect<Equal<Trunc<1.234>, '1'>>,
