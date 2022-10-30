0032c34f3d7dcfdf332836e7262452222f6d07a0
Zhentian Wan <answer881215@gmail.com>
2022-10-30 10:41:37 +0200

    Update array.ts

--- src/types/array.ts
+++ src/types/array.ts
@@ -1,4 +1,21 @@
 import type { Equal } from './utils';
+
+/*
+Example
+type cases = [
+  Expect<Equal<ConstructTuple<0>, []>>,
+  Expect<Equal<ConstructTuple<2>, [unknown, unknown]>>,
+  Expect<Equal<ConstructTuple<999>['length'], 999>>,
+  // @ts-expect-error
+  Expect<Equal<ConstructTuple<1000>['length'], 1000>>,
+]
+*/
+export type ConstructTuple<L extends number, ACC extends unknown[] = []> = L extends 0
+  ? []
+  : ACC['length'] extends L 
+    ? ACC
+    : ConstructTuple<L, [...ACC, unknown]>;
+
 /*
 Example
 type cases = [
