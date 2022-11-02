979704f2f991366f6d31fc062523b9893559e50a
Zhentian Wan <answer881215@gmail.com>
2022-11-02 08:32:58 +0200

    Update utils.ts

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -17,3 +17,58 @@ export type BEM<B extends string, E extends string[], M extends string[]> = M['l
   : E['length'] extends 0
     ? `${B}--${M[number]}`
     : `${B}__${E[number]}--${M[number]}`;
+    
+/*
+ Example
+ type PersonInfo = {
+  name: 'Tom'
+  age: 30
+  married: false
+  addr: {
+    home: '123456'
+    phone: '13111111111'
+  },
+  hobbies: ['swim', 'sing']
+}
+
+type ExpectedResult = {
+  name: string
+  age: number
+  married: boolean
+  addr: {
+    home: string
+    phone: string
+  }
+  hobbies: [string, string]
+}
+
+type cases = [
+  Expect<Equal<ToPrimitive<PersonInfo>, ExpectedResult>>,
+]
+*/
+type ArrayToPrimitive<T extends any[], ACC extends any[] = []> = T extends [infer F, ...infer RT] 
+  ? [ValueToPrimitive<F>, ...ArrayToPrimitive<RT>] 
+  : ACC;
+type ObjectToPrimitive<T extends Record<PropertyKey, any>> = {
+  [Key in keyof T]: T[Key] extends Record<PropertyKey, any> 
+                      ? ObjectToPrimitive<T[Key]> 
+                      : ValueToPrimitive<T[Key]>
+}
+type ValueToPrimitive<T extends any> = T extends string 
+  ? string 
+  : T extends number 
+    ? number
+    : T extends boolean 
+      ? boolean
+      : T extends undefined 
+        ? undefined
+        : T extends null 
+          ? null
+          : never;
+export type ToPrimitive<T extends Record<PropertyKey, any>> = {
+  [Key in keyof T]: T[Key] extends any[] 
+                      ? ArrayToPrimitive<T[Key]> 
+                      : T[Key] extends object 
+                        ? ObjectToPrimitive<T[Key]>
+                        : ValueToPrimitive<T[Key]>
+}
