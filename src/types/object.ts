86ddce5729b34c5fac6e455137dea8c52f119745
Zhentian Wan <answer881215@gmail.com>
2022-11-05 11:27:24 +0200

    Update object.ts

--- src/types/object.ts
+++ src/types/object.ts
@@ -1,5 +1,63 @@
 /* Object */
 
+/*
+Example
+type cases = [
+  Expect<Equal<DeepReadonly<X>, Expected>>,
+]
+
+type X = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type Expected = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+}
+*/
+export type DeepReadonly<T extends Record<PropertyKey, any>> = T extends (...args: any[]) => any
+  ? T
+  : {
+    readonly [Key in keyof T]: DeepReadonly<T[Key]>
+  };
+
 export type DeepPartial<T> = T extends Function
   ? T
   : T extends Array<infer InferredArrayMember>
