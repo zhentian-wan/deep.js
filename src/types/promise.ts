cdb32889b32b1ba741a42a09d8f21fe77c42d53c
Zhentian Wan <answer881215@gmail.com>
2022-11-06 10:30:47 +0200

    Create promise.ts

---
+++ src/types/promise.ts
@@ -0,0 +1,14 @@
+/*
+Example
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const)
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const)
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)])
+
+type cases = [
+  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
+]
+*/
+export declare function PromiseAll<T extends any[]>(values: readonly [...T]): Promise<{[Key in keyof T]: T[Key] extends Promise<infer V> ? V : T[Key]}>;
+
