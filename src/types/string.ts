fe318b2a33f2e284722536ef1231fcc45c3a517a
Zhentian Wan <answer881215@gmail.com>
2022-11-08 08:52:57 +0200

    replaceAll string

--- src/types/string.ts
+++ src/types/string.ts
@@ -97,6 +97,26 @@ export type TrimLeft<S extends string> = S extends `${Space}${infer RT}`
 /*
 Example
 type cases = [
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
+]
+*/
+export type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+  ? S
+  : S extends `${infer A}${From}${infer B}`
+    ? `${A}${To}${ReplaceAll<B, From, To>}`
+    : S;
+
+/*
+Example
+type cases = [
   Expect<Equal<TrimRight<'str'>, 'str'>>,
   Expect<Equal<TrimRight<'str  '>, 'str'>>,
   Expect<Equal<TrimRight<'str     '>, 'str'>>,
