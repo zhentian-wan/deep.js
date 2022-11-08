6fa31fc6bada3b799b3d3d4a595954e38d2554ca
Zhentian Wan <answer881215@gmail.com>
2022-11-08 08:46:25 +0200

    replace string

--- src/types/string.ts
+++ src/types/string.ts
@@ -45,6 +45,23 @@ export type Permutations<S extends string> = Combinations<StringToUnion<S>>;
 /*
 Example
 type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
+]
+*/
+export type Replace<S extends string, From extends string, To extends string> = From extends '' 
+? S
+: S extends `${infer A}${From}${infer B}`
+  ? `${A}${To}${B}`
+  : S;
+
+/*
+Example
+type cases = [
   Expect<Equal<Trim<'str'>, 'str'>>,
   Expect<Equal<Trim<' str'>, 'str'>>,
   Expect<Equal<Trim<'     str'>, 'str'>>,
