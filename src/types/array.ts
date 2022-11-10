9ecd9b7a75c068723b561a5beaf6a1519b803080
Zhentian Wan <answer881215@gmail.com>
2022-11-10 13:55:09 +0200

    Permutation

--- src/types/array.ts
+++ src/types/array.ts
@@ -270,6 +270,22 @@ type cases = [
 export type Pop<T extends any[]> = T extends [...infer RT, infer L] ? RT : [];
 
 /*
+Example:
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>,
+]
+*/
+export type Permutation<T, U = T> = [T] extends [never] 
+  ? [] 
+  : U extends any
+    ? [U, ...Permutation<Exclude<T, U>>]
+    : [];
+
+/*
 Example
 type cases = [
   Expect<Equal<Reverse<[]>, []>>,
