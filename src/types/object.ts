431353727d2fda2a4837a2714b11dd6b06339489
Zhentian Wan <answer881215@gmail.com>
2022-11-12 11:03:21 +0200

    AppendToObjet

--- src/types/object.ts
+++ src/types/object.ts
@@ -2,6 +2,55 @@
 
 /*
 Example
+type test1 = {
+  key: 'cat'
+  value: 'green'
+}
+
+type testExpect1 = {
+  key: 'cat'
+  value: 'green'
+  home: boolean
+}
+
+type test2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+}
+
+type testExpect2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+  home: 1
+}
+
+type test3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+}
+
+type testExpect3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+  isMotherRussia: false | undefined
+}
+
+type cases = [
+  Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
+  Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
+  Expect<Equal<AppendToObject<test3, 'isMotherRussia', false | undefined>, testExpect3>>,
+]
+*/
+export type AppendToObject<T extends Record<PropertyKey, any>, U extends string | number | symbol, V extends any> = MergeObject<T & {
+  [Key in U]: V  
+}>
+
+/*
+Example
 type cases = [
   Expect<Equal<DeepReadonly<X>, Expected>>,
 ]
