0f26850ad65996eed6413924acba5d0d554b6661
Zhentian Wan <answer881215@gmail.com>
2022-11-13 18:26:51 +0200

    Absolute

--- src/types/number.ts
+++ src/types/number.ts
@@ -1,6 +1,25 @@
 /*
 Example
 type cases = [
+  Expect<Equal<Absolute<0>, '0'>>,
+  Expect<Equal<Absolute<-0>, '0'>>,
+  Expect<Equal<Absolute<10>, '10'>>,
+  Expect<Equal<Absolute<-5>, '5'>>,
+  Expect<Equal<Absolute<'0'>, '0'>>,
+  Expect<Equal<Absolute<'-0'>, '0'>>,
+  Expect<Equal<Absolute<'10'>, '10'>>,
+  Expect<Equal<Absolute<'-5'>, '5'>>,
+  Expect<Equal<Absolute<-1_000_000n>, '1000000'>>,
+  Expect<Equal<Absolute<9_999n>, '9999'>>,
+]
+*/
+export type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer S}`
+  ? S
+  : `${T}`;
+
+/*
+Example
+type cases = [
   Expect<Equal<MinusOne<1>, 0>>,
   Expect<Equal<MinusOne<55>, 54>>,
   Expect<Equal<MinusOne<3>, 2>>,
