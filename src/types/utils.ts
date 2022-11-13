0ac367bb09e77c4e4814136181c55a6274bfde7f
Zhentian Wan <answer881215@gmail.com>
2022-11-13 18:52:08 +0200

    IsNever

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -147,3 +147,17 @@ S extends `/${string}/:${infer Param}/${infer REST}`
   : S extends `${string}/:${infer Param}`
     ? Param
     : never;
+
+/*
+Example
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>,
+]
+*/
+export type IsNever<T> = [T] extends [never] ? true: false;
