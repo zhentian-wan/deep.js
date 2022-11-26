9d51768c3cfd9c3b23cb0b792960e99ea8e2b85c
Zhentian Wan <answer881215@gmail.com>
2022-11-26 10:16:22 +0200

    ClassPublicKeys

--- src/types/class.ts
+++ src/types/class.ts
@@ -8,3 +8,26 @@ class Person {
 Properties<typeof Person> // 'name' | 'age'
 */
 export type Properties<T extends {new (...args: any[]): any}> = keyof InstanceType<T>
+
+/* 
+Example
+class A {
+  public str: string
+  protected num: number
+  private bool: boolean
+  constructor() {
+    this.str = 'naive'
+    this.num = 19260917
+    this.bool = true
+  }
+
+  getNum() {
+    return Math.random()
+  }
+}
+
+type cases = [
+  Expect<Equal<ClassPublicKeys<A>, 'str' | 'getNum'>>,
+]
+*/
+export type ClassPublicKeys<C> = keyof C;
