666593a6bd81584f6f6bad2d86b47182d17e9543
Zhentian Wan <answer881215@gmail.com>
2022-12-03 14:41:55 +0200

    Update object.ts

--- src/types/object.ts
+++ src/types/object.ts
@@ -652,6 +652,62 @@ export type ObjectFromEntries<T extends any[]> = {
 
 /*
 Example
+const ref = {
+  count: 1,
+  person: {
+    name: 'cattchen',
+    age: 22,
+    books: ['book1', 'book2'],
+    pets: [
+      {
+        type: 'cat',
+      },
+    ],
+  },
+}
+
+type cases = [
+  Expect<Equal<ObjectKeyPaths<{ name: string; age: number }>, 'name' | 'age'>>,
+  Expect<
+  Equal<
+  ObjectKeyPaths<{
+    refCount: number
+    person: { name: string; age: number }
+  }>,
+  'refCount' | 'person' | 'person.name' | 'person.age'
+  >
+  >,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'count'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.name'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.age'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.pets'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books.0'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books.1'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books[0]'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books.[0]'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.pets.0.type'>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'notExist'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.notExist'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.name.'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, '.person.name'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.pets.[0]type'>, false>>,
+]
+*/
+export type GenNode<K extends string | number, IsRoot extends boolean> = IsRoot extends true? `${K}`: `.${K}` | (K extends number? `[${K}]` | `.[${K}]`:never)
+
+export type ObjectKeyPaths<
+  T extends object,
+  IsRoot extends boolean = true,
+  K extends keyof T = keyof T
+> = 
+K extends string | number ?
+  GenNode<K,IsRoot> | (T[K] extends object? `${GenNode<K,IsRoot>}${ObjectKeyPaths<T[K],false>}`:never)
+  :never;
+
+/*
+Example
 type cases = [
   Expect<Equal<OptionalKeys<{ a: number; b?: string }>, 'b'>>,
   Expect<Equal<OptionalKeys<{ a: undefined; b?: undefined }>, 'b'>>,
