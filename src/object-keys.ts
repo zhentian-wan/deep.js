f3dac3efb82c2470de90e37190923cd2bab95c81
Zhentian Wan <answer881215@gmail.com>
2023-01-15 15:13:42 +0100

    second solution

--- src/object-keys.ts
+++ src/object-keys.ts
@@ -1,3 +1,9 @@
 export const typedObjectKeys = <Keys extends string>(obj: Record<Keys, any>) => {
     return Object.keys(obj) as Array<Keys>;
 };
+
+/**
+export const typedObjectKeys = <T extends object>(obj: T) => {
+    return Object.keys(obj) as Array<keyof T>;
+};
+*/
