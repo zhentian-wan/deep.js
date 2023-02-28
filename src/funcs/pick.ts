cf14c2c817b736cb25e113e52d97e7698e3a2732
Zhentian Wan <answer881215@gmail.com>
2023-02-28 10:11:00 +0200

    save

--- src/funcs/pick.ts
+++ src/funcs/pick.ts
@@ -1,40 +1,40 @@
-import { expect, it } from 'vitest';
-import { Equal, Expect } from '../types/type-utils';
+import { expect, it } from "vitest";
+import { Equal, Expect } from "../types/utils";
 
-export const pick = <T, Keys extends Array<keyof T>>(obj: T, picked: Keys) => {
-    return picked.reduce((acc, key) => {
-      acc[key] = obj[key];
-      return acc;
-    }, {} as { [Key in Keys[number]]: T[Key] });
-  };
+export const pick = <T, Keys extends keyof T>(obj: T, picked: Array<Keys>) => {
+  return picked.reduce((acc, key) => {
+    acc[key] = obj[key];
+    return acc;
+  }, {} as Pick<T, Keys>);
+};
 
-  it('Should pick the keys from the object', () => {
-    const result = pick(
-      {
-        a: 1,
-        b: 2,
-        c: 3,
-      },
-      ['a', 'b']
-    );
-  
-    expect(result).toEqual({ a: 1, b: 2 });
-  
-    type test = Expect<Equal<typeof result, { a: number; b: number }>>;
-  });
-  
-  it('Should not allow you to pass keys which do not exist in the object', () => {
-    pick(
-      {
-        a: 1,
-        b: 2,
-        c: 3,
-      },
-      [
-        'a',
-        'b',
-        // @ts-expect-error
-        'd',
-      ]
-    );
-  });
+it("Should pick the keys from the object", () => {
+  const result = pick(
+    {
+      a: 1,
+      b: 2,
+      c: 3,
+    },
+    ["a", "b"]
+  );
+
+  expect(result).toEqual({ a: 1, b: 2 });
+
+  type test = Expect<Equal<typeof result, { a: number; b: number }>>;
+});
+
+it("Should not allow you to pass keys which do not exist in the object", () => {
+  pick(
+    {
+      a: 1,
+      b: 2,
+      c: 3,
+    },
+    [
+      "a",
+      "b",
+      // @ts-expect-error
+      "d",
+    ]
+  );
+});
