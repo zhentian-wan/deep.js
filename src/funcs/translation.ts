b18c4b37909c4cdffb7c3dd67c44c84399a30a1c
Zhentian Wan <answer881215@gmail.com>
2023-03-02 09:29:31 +0200

    translation

---
+++ src/funcs/translation.ts
@@ -0,0 +1,58 @@
+import { expect, it } from 'vitest';
+
+type GetParamKeys<TTranslation extends string> = TTranslation extends ''
+  ? []
+  : TTranslation extends `${string}{${infer Param}}${infer Tail}`
+  ? [Param, ...GetParamKeys<Tail>]
+  : [];
+
+type GetParamKeysAsUnion<TTranslation extends string> =
+  GetParamKeys<TTranslation>[number];
+
+const translate = <
+  TTrans extends Record<string, string>,
+  TKey extends keyof TTrans
+>(
+  translations: TTrans,
+  key: TKey,
+  ...args: GetParamKeysAsUnion<TTrans[TKey]> extends never
+    ? [arg?: undefined]
+    : [arg: Record<GetParamKeysAsUnion<TTrans[TKey]>, string>]
+) => {
+  const translation = translations[key];
+  const params: any = args[0] || {};
+
+  return translation.replace(/{(\w+)}/g, (_, key) => params[key]);
+};
+
+// TESTS
+
+const translations = {
+  title: 'Hello, {name}!',
+  subtitle: 'You have {count} unread messages.',
+  button: 'Click me!',
+} as const;
+
+it('Should translate a translation without parameters', () => {
+  const buttonText = translate(translations, 'button');
+
+  expect(buttonText).toEqual('Click me!');
+});
+
+it('Should translate a translation WITH parameters', () => {
+  const subtitle = translate(translations, 'subtitle', {
+    count: '2',
+  });
+
+  expect(subtitle).toEqual('You have 2 unread messages.');
+});
+
+it('Should force you to provide parameters if required', () => {
+  // @ts-expect-error
+  translate(translations, 'title');
+});
+
+it('Should not let you pass parameters if NOT required', () => {
+  // @ts-expect-error
+  translate(translations, 'button', {});
+});
