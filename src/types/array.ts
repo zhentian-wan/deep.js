66b87137098b39c0235329164c3294cfc82179d9
Zhentian Wan <answer881215@gmail.com>
2023-04-04 21:22:21 +0300

    transpose

--- src/types/array.ts
+++ src/types/array.ts
@@ -504,6 +504,23 @@ export type TupleToObject<T extends ReadonlyArray<string | number>> = {
   [Key in T[number]]: Key;
 };
 
+/**
+ type cases = [
+  Expect<Equal<Transpose<[]>, []>>,
+  Expect<Equal<Transpose<[[1]]>, [[1]]>>,
+  Expect<Equal<Transpose<[[1, 2]]>, [[1], [2]]>>,
+  Expect<Equal<Transpose<[[1, 2], [3, 4]]>, [[1, 3], [2, 4]]>>,
+  Expect<Equal<Transpose<[[1, 2, 3], [4, 5, 6]]>, [[1, 4], [2, 5], [3, 6]]>>,
+  Expect<Equal<Transpose<[[1, 4], [2, 5], [3, 6]]>, [[1, 2, 3], [4, 5, 6]]>>,
+  Expect<Equal<Transpose<[[1, 2, 3], [4, 5, 6], [7, 8, 9]]>, [[1, 4, 7], [2, 5, 8], [3, 6, 9]]>>,
+]
+ */
+export type Transpose<M extends number[][],R = M['length'] extends 0?[]:M[0]> = {
+  [X in keyof R]:{
+    [Y in keyof M]:X extends keyof M[Y]?M[Y][X]:never
+  }
+}
+
 /*
 Example
 type cases = [
