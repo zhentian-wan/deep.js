263bc6318b5aa278e4f1d57077274570285a9241
Zhentian Wan <answer881215@gmail.com>
2023-04-09 16:57:09 +0300

    save

---
+++ src/execs/object-to-discriminated-union.ts
@@ -0,0 +1,33 @@
+import {Expect, Euqal} from "../types/utils"
+
+interface Attributes {
+    id: string;
+    email: string;
+    username: string;
+  }
+  
+  type MutuallyExclusive<T> = {
+    [Key in keyof T]: {
+      [P in Key]: T[P]
+    }
+  }[keyof T];
+  
+  type ExclusiveAttributes = MutuallyExclusive<Attributes>;
+  
+  type tests = [
+    Expect<
+      Equal<
+        ExclusiveAttributes,
+        | {
+            id: string;
+          }
+        | {
+            email: string;
+          }
+        | {
+            username: string;
+          }
+      >
+    >,
+  ];
+  
