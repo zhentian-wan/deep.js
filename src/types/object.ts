a13afd81a3f59bcb0002b29793ae451c359016cf
Zhentian Wan <answer881215@gmail.com>
2023-07-16 21:38:53 +0300

    Update object.ts

--- src/types/object.ts
+++ src/types/object.ts
@@ -630,6 +630,47 @@ export type Merge<
     : never;
 };
 
+/**
+Example:
+type cases = [
+  Expect<Equal<MergeAll<[]>, {} >>,
+  Expect<Equal<MergeAll<[{ a: 1 }]>, { a: 1 }>>,
+  Expect<Equal<
+    MergeAll<[{ a: string }, { a: string }]>,
+    { a: string }>
+  >,
+  Expect<Equal<
+    MergeAll<[{ }, { a: string }]>,
+    { a: string }>
+  >,
+  Expect<Equal<
+    MergeAll<[{ a: 1 }, { c: 2 }]>,
+    { a: 1; c: 2 }>
+  >,
+  Expect<Equal<
+    MergeAll<[{ a: 1; b: 2 }, { a: 2 }, { c: 3 }]>,
+    { a: 1 | 2; b: 2; c: 3 }>
+  >,
+  Expect<Equal<MergeAll<[{ a: 1 }, { a: number }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: number }, { a: 1 }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: 1 | 2 }, { a: 1 | 3 }]>, { a: 1 | 2 | 3 }>>,
+]
+*/
+/* _____________ Your Code Here _____________ */
+export type MergeValueAsUnion<
+  F extends object,
+  S extends object
+> = {
+  [Key in  (keyof F | keyof S)]: Key extends keyof S 
+    ? S[Key] | (Key extends keyof F ? F[Key] : never)
+    : Key extends keyof F
+      ? F[Key]
+      : never
+};
+export type MergeAll<XS extends object[], ACC extends object = {}> = XS extends [infer A extends object, ...infer REST extends object[]]
+  ? MergeAll<REST, MergeValueAsUnion<ACC, A>>
+  : ACC;
+
 /*
 Example:
 interface Model {
