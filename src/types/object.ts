570ddb9af3d851977cdd6cffcfda4cd14330e8bd
Zhentian Wan <answer881215@gmail.com>
2023-07-18 14:57:55 +0300

    Update object.ts

--- src/types/object.ts
+++ src/types/object.ts
@@ -711,6 +711,34 @@ export type OmitByType<T extends object, U> = {
 
 /*
 Example
+type cases = [
+  Expect<Equal<OptionalUndefined<{ value: string | undefined }, 'value'>, { value?: string | undefined }>>,
+  Expect<Equal<OptionalUndefined<{ value: string; desc: string }, 'value'>, { value: string; desc: string }>>,
+  Expect<Equal<OptionalUndefined<{ value: string | undefined; desc: string }, 'value'>, { value?: string; desc: string }>>,
+  Expect<Equal<OptionalUndefined<{ value: string | undefined; desc: string | undefined }, 'value'>, { value?: string | undefined; desc: string | undefined }>>,
+  Expect<Equal<OptionalUndefined<{ value: string | undefined; desc: string }, 'value' | 'desc'>, { value?: string; desc: string }>>,
+  Expect<Equal<OptionalUndefined<{ value: string | undefined; desc: string | undefined }>, { value?: string; desc?: string }>>,
+  Expect<Equal<OptionalUndefined<{ value?: string }, 'value'>, { value?: string }>>,
+  Expect<Equal<OptionalUndefined<{ value?: string }>, { value?: string }>>,
+]
+*/
+export type OptionalUndefined<
+  T, 
+  Props extends keyof T = keyof T,
+  OptionsProps extends keyof T = Props extends keyof T
+    ? undefined extends T[Props]
+      ? Props
+      :never 
+    :never
+> = 
+  MergeObject<{
+    [K in OptionsProps]?: T[K]
+  } & {
+    [K in Exclude<keyof T, OptionsProps>]: T[K]
+  }>;
+
+/*
+Example
 interface User {
   name?: string
   age?: number
