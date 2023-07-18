34eaa6307d14c265a5e379d598a144c4c5b28094
Zhentian Wan <answer881215@gmail.com>
2023-07-18 14:24:05 +0300

    CartesianProduct

--- src/types/union.ts
+++ src/types/union.ts
@@ -1,3 +1,18 @@
+/**
+ * Example
+ type cases = [
+  Expect<Equal<CartesianProduct<1 | 2, 'a' | 'b'>, [2, 'a'] | [1, 'a'] | [2, 'b'] | [1, 'b']>>,
+  Expect<Equal<CartesianProduct<1 | 2 | 3, 'a' | 'b' | 'c' >, [2, 'a'] | [1, 'a'] | [3, 'a'] | [2, 'b'] | [1, 'b'] | [3, 'b'] | [2, 'c'] | [1, 'c'] | [3, 'c']>>,
+  Expect<Equal<CartesianProduct<1 | 2, 'a' | never>, [2, 'a'] | [1, 'a'] >>,
+  Expect<Equal<CartesianProduct<'a', Function | string>, ['a', Function] | ['a', string]>>,
+]
+*/
+export type CartesianProduct<T, U> = T extends infer T1
+  ? U extends infer U1
+    ? [T1, U1] | CartesianProduct<Exclude<T, T1>, Exclude<U, U1>>
+    : never
+  : never;
+
 /*
 Example
 Equal<UnionToIntersectionFn<'a' | 'b' | 'c'>, (() => "a") & (() => "b") & (() => "c")>
