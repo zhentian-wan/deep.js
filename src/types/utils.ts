855678607b393cd08b9790d72627446fcdc5d505
Zhentian Wan <answer881215@gmail.com>
2024-05-12 20:36:57 +0300

    Update utils.ts

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -392,6 +392,26 @@ export type NumberToString<T extends number> = `${T}`;
 export type NotNil<T> = T extends object ? true: false;
 export type IsNil<T> = T extends object ? false: true;
 /*
+Example: IsUnion
+type cases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | 'a'>, false>>,
+  Expect<Equal<IsUnion<never>, false>>,
+]
+*/
+export type IsUnion<T> = NotEqual<[T] & T,(T extends T ? [T] : never) & T>;
+
+/*
 Example: ObjectToUnion
 interface Attributes {
   id: string;
