42c42a8475cf81fc99a47256acab2cb80f491c83
Zhentian Wan <answer881215@gmail.com>
2024-05-13 22:26:56 +0300

    Update string.ts

--- src/types/string.ts
+++ src/types/string.ts
@@ -280,3 +280,65 @@ type cases = [
 export type TrimRight<S extends string> = S extends `${infer Left}${Space}`
   ? TrimRight<Left>
   : S;
+
+/*
+Example IsFixedStringLiteralType
+type testcase =
+  | Expect<Equal<IsFixedStringLiteralType<'ABC'>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<string>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<'ABC' | 'DEF'>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<never>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${string}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${string & {}}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${number}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${bigint}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${boolean}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${true}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${false}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${null}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${undefined}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${string}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${string & {}}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${number}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${bigint}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${boolean}`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${true}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${false}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${null}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${undefined}`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${string}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${string & {}}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${number}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${bigint}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${boolean}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`${true}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${false}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${null}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`${undefined}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${string}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${string & {}}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${number}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${bigint}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${boolean}DEF`>, false>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${true}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${false}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${null}DEF`>, true>>
+  | Expect<Equal<IsFixedStringLiteralType<`ABC${undefined}DEF`>, true>>
+  | true
+/*
+export type IsFixedNonNumericString<S> = S extends ''
+  ? true
+  : S extends `${infer C}${infer T}`
+    ? '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' extends C
+      ? false
+      : IsFixedNonNumericString<T>
+    : false;
+export type IsFixedStringLiteralType<S extends string, T = S> = [S] extends [never]
+  ? false
+  : S extends unknown
+    ? [T] extends [S]
+      ? IsFixedNonNumericString<S>
+      : false
+    : false;
+
+
