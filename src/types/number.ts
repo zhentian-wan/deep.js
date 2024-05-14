c1271c749d9c4e121434dcea2bf6cf531ed293f6
Zhentian Wan <answer881215@gmail.com>
2024-05-14 22:30:11 +0300

    Update number.ts

--- src/types/number.ts
+++ src/types/number.ts
@@ -110,6 +110,25 @@ export type Trunc<N extends number | string> = `${N}` extends `${infer NUM}.${in
   ? `${NUM}`
   : `${N}`;
 
+/*
+Example IsOdd
+type cases = [
+  Expect<Equal<IsOdd<5>, true>>,
+  Expect<Equal<IsOdd<2023>, true>>,
+  Expect<Equal<IsOdd<1453>, true>>,
+  Expect<Equal<IsOdd<1926>, false>>,
+  Expect<Equal<IsOdd<2.3>, false>>,
+  Expect<Equal<IsOdd<3e23>, false>>,
+  Expect<Equal<IsOdd<number>, false>>,
+]
+*/
+export type IsOdd<T extends number> = `${T}` extends `${infer _}.${infer __}` 
+  ? false 
+  :`${T}` extends `${infer _}e${infer __}` 
+    ? false
+    : LastChar<`${T}`> extends '1' | '3' | '5' | '7' | '9' 
+      ? true 
+      : false; 
 
 /**
  * Comparator<T, U>
@@ -165,23 +184,3 @@ export type Comparator<A extends number, B extends number> =
             ? Comparison.Lower
             : Compare<BB & number, AA & number>
       : never;
-
-/*
-Example IsOdd
-type cases = [
-  Expect<Equal<IsOdd<5>, true>>,
-  Expect<Equal<IsOdd<2023>, true>>,
-  Expect<Equal<IsOdd<1453>, true>>,
-  Expect<Equal<IsOdd<1926>, false>>,
-  Expect<Equal<IsOdd<2.3>, false>>,
-  Expect<Equal<IsOdd<3e23>, false>>,
-  Expect<Equal<IsOdd<number>, false>>,
-]
-*/
-export type IsOdd<T extends number> = `${T}` extends `${infer _}.${infer __}` 
-  ? false 
-  :`${T}` extends `${infer _}e${infer __}` 
-    ? false
-    : LastChar<`${T}`> extends '1' | '3' | '5' | '7' | '9' 
-      ? true 
-      : false; 
