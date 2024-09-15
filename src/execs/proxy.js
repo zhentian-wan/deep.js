e44b75c34809848895266f5aeeb8c1033f48709f
zhentian-wan <answer881215@gmail.com>
2024-09-15 14:36:06 +0300

    chore: save

--- src/execs/proxy.js
+++ src/execs/proxy.js
@@ -11,6 +11,6 @@ const add = new Proxy({
     }
 });
 
-const res1 = add[1][2][3] + 4; // 10
-const res2 = add[10][20][30] + 40; // 100
-const res3 = add[100][200][300] + 400; // 1000
+// const res1 = add[1][2][3] + 4; // 10
+// const res2 = add[10][20][30] + 40; // 100
+// const res3 = add[100][200][300] + 400; // 1000
